8db2e6701b75035381f0a1b2be844d890b08eade
Gustavo Tavares <gustavarees@gmail.com>
2020-07-07 22:30:45 -0300

    Primeiro commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
     function mudarpaginaum() {
-                window.location.href = 'C:/Users/gusta/OneDrive/Documentos/Show do Milhão/questdois.html';
-
+      window.location.href = 'C:/Users/gusta/OneDrive/Documentos/Show do Milhão/questdois.html';
     } 
 
     function habilitaBtn () {
@@ -8,3 +7,20 @@
       document.getElementById('avancar').disabled=false;
          
     }
+
+    function mudarpaginaderrota() {
+      window.location.href = 'C:/Users/gusta/OneDrive/Documentos/Show do Milhão/loseum.html';
+    }
+
+    function mudarpaginaderrotadois() {
+      window.location.href = 'C:/Users/gusta/OneDrive/Documentos/Show do Milhão/losedois.html';
+    }
+
+    function mudarpaginaparar() {
+      window.location.href = 'C:/Users/gusta/OneDrive/Documentos/Show do Milhão/pararum.html';
+    }
+
+    function mudarpaginaparardois() {
+      window.location.href = 'C:/Users/gusta/OneDrive/Documentos/Show do Milhão/parardois.html';
+    }
+
