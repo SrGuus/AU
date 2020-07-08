1b9b3b12985d767dfa1bb0fb9ba88fa232b791fb
Gustavo Tavares <gustavarees@gmail.com>
2020-07-07 21:37:59 -0300

    Primeiro commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+    function mudarpaginaum() {
+                window.location.href = 'C:/Users/gusta/OneDrive/Documentos/Show do Milhão/questdois.html';
+
+    } 
+
+    function habilitaBtn () {
+    		
+      document.getElementById('avancar').disabled=false;
+         
+    }
